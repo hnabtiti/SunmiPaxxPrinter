d2d3c2f8992f6d56dd7645832c2d1c526ff624b1
Edoardo <edoardogullo@libero.it>
2020-09-16 15:52:38 +0200

    init

--- SunmiPaxxPrinterLibrary/src/main/java/com/isi/sunmipaxxprinterlibrary/PrinterPaxx.java
+++ SunmiPaxxPrinterLibrary/src/main/java/com/isi/sunmipaxxprinterlibrary/PrinterPaxx.java
@@ -2,8 +2,6 @@ package com.isi.sunmipaxxprinterlibrary;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.util.Log;
-
 import com.pax.dal.IDAL;
 import com.pax.dal.IPrinter;
 import com.pax.dal.exceptions.PrinterDevException;
@@ -11,18 +9,16 @@ import com.pax.gl.page.IPage;
 import com.pax.gl.page.PaxGLPage;
 import com.pax.neptunelite.api.NeptuneLiteUser;
 
+import java.util.ArrayList;
+
 public class PrinterPaxx implements Printer {
 
-    private PaxGLPage paxGLPage;
-    private IPage myPage;
-    private IPage.ILine line;
     private Context c;
+    private ArrayList<PaxLine> lines = new ArrayList<>();
 
     PrinterPaxx(Context c) {
 
-        paxGLPage = PaxGLPage.getInstance(c);
-        myPage = paxGLPage.createPage();
-        line = myPage.addLine();
+
         this.c = c;
 
     }
@@ -61,23 +57,33 @@ public class PrinterPaxx implements Printer {
                 break;
         }
 
+        PaxLine line = new PaxLine(text, dimension, align, styleInner);
 
-
-        line.addUnit(text, 35, align, styleInner);
-        line = myPage.addLine();
+        lines.add(line);
 
     }
 
     @Override
     public void addSpace() {
-        line.addUnit(" ", 25, IPage.EAlign.CENTER, IPage.ILine.IUnit.TEXT_STYLE_NORMAL);
-        line = myPage.addLine();
+        PaxLine line = new PaxLine(" ", 30, IPage.EAlign.CENTER, IPage.ILine.IUnit.TEXT_STYLE_NORMAL);
+
+        lines.add(line);
     }
 
     @Override
     public void printText() {
 
-        Log.e("Number of lines: ", "printText: " +myPage.getLines().size());
+        PaxGLPage  paxGLPage = PaxGLPage.getInstance(c);
+        IPage myPage = paxGLPage.createPage();
+        IPage.ILine line = myPage.addLine();
+
+        for (PaxLine lineIn: lines) {
+
+            line.addUnit(lineIn.getText(), lineIn.getSize(), lineIn.getAlign(), lineIn.getStyle());
+            line = myPage.addLine();
+
+        }
+
 
         Bitmap pageBitmap = paxGLPage.pageToBitmap(myPage, 384);
 
@@ -129,5 +135,36 @@ public class PrinterPaxx implements Printer {
             e.printStackTrace();
         }
 
+    }
+
+    private static class PaxLine{
+
+        private String text;
+        private int size;
+        private IPage.EAlign align;
+        private int style;
+
+        public PaxLine(String text, int size, IPage.EAlign align, int style) {
+            this.text = text;
+            this.size = size;
+            this.align = align;
+            this.style = style;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public IPage.EAlign getAlign() {
+            return align;
+        }
+
+        public int getStyle() {
+            return style;
+        }
     }
 }
